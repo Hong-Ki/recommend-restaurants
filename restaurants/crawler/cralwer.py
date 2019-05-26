3a69b9a8eea5889b0152f1bb47b4dd0191ad09d2
Minong <minhk3259@gmail.com>
2019-05-26 17:11:49 +0900

    add_chrome_path

--- restaurants/crawler/cralwer.py
+++ restaurants/crawler/cralwer.py
@@ -2,20 +2,24 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import os
 
-driver_url = os.getenv('GOOGLE_CHROME_SHIM')
-if driver_url == None:
-    dir = os.path.dirname(os.path.abspath(__file__))
-    driver_url = os.path.join(dir, 'chromedriver')
-
-_driver_url = driver_url
+_driver_url = os.getenv(
+    'CHROME_PATH',
+    os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'chromedriver'
+    )
+)
 _search_url = 'https://store.naver.com/restaurants/list?entry=pll&query='
 
 
 def parsingRestaurnts(query):
     options = webdriver.ChromeOptions()
+    options.binary_location = os.getenv('CHROME_BIN', None)
     options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
     options.add_argument("disable-gpu")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
 
     driver = webdriver.Chrome(_driver_url, chrome_options=options)
     driver.get(_search_url + query)
