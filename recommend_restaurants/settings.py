9fd0662d817339d0863933398f0b41c9dee47d75
Minong <minhk3259@gmail.com>
2019-05-26 16:58:08 +0900

    update_allowed_host

--- recommend_restaurants/settings.py
+++ recommend_restaurants/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 LOGGING = {
