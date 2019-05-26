0777934a6b901d4e14df39a1acfff187b1f5c294
Minong <minhk3259@gmail.com>
2019-05-26 20:56:31 +0900

    test

--- restaurants/crawler/cralwer.py
+++ restaurants/crawler/cralwer.py
@@ -21,6 +21,8 @@ def parsingRestaurnts(query):
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
 
+    print(os.path.dirname(os.path.abspath(__file__)))
+
     driver = webdriver.Chrome(_driver_url, chrome_options=options)
     driver.get(_search_url + query)
     html = driver.page_source
